d4af900f43f6fe0b43d7c0e86558e8929d127b5b
MagdalenaPamulaPosz <mpamula92@gmail.com>
2025-12-07 18:44:28 +0100

    Setup project

---
+++ NQueens/NQueens/NQueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NQueensApp.swift
+//  NQueens
+//
+//  Created by Magdalena Pamuła-Posz on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NQueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
