31d243973bcb6623b69cc358bcbf2573afdc2ee5
MagdalenaPamulaPosz <mpamula92@gmail.com>
2025-12-07 19:09:44 +0100

    Update GameEngine to have a possibility of reset and remove

--- NQueens/NQueens/GameEngine.swift
+++ NQueens/NQueens/GameEngine.swift
@@ -18,8 +18,12 @@ protocol GameEngine {
     init(boardSize: Int)
     
     var currentBoard: [[Int]] { get }
+    var boardSize: Int { get }
+    var occupiedPosistions: [Position] { get }
     
-    func place(figure: Figure) throws(PlacementError) -> PlacementResult
+    func place(figure: Figure) throws -> PlacementResult
+    func remove(at position: Position) -> PlacementResult
+    func reset()
 }
 
 struct Position: Equatable {
@@ -27,70 +31,75 @@ struct Position: Equatable {
     let y: Int
 }
 
-protocol Figure {
-    var position: Position { get }
-    var availableMovementFields: ([[Int]]) -> [[Int]] { get }
-}
-
-class QueenFigure: Figure {
-    var position: Position
-    
-    init(position: Position) {
-        self.position = position
-    }
-    
-    lazy var availableMovementFields: ([[Int]]) -> [[Int]] = { [position] matrix in
-        var result = matrix
-        let size = matrix.count
-        
-        let x = Int(position.x)
-        let y = Int(position.y)
-        
-        // Horizontal and Vertical
-        for i in 0..<size {
-            if i != y { result[x][i] = 1 }
-            if i != x { result[i][y] = 1 }
-        }
-        
-        // Diagonals
-        for i in 0..<size {
-            for j in 0..<size {
-                if abs(i - x) == abs(j - y) && !(i == x && j == y) {
-                    result[i][j] = 1
-                }
-            }
-        }
-        
-        return result
-    }
-}
-
 class GameEngineImpl: GameEngine {
     var figures: [Figure] = []
     var boardMatrix: [[Int]]
-    var currentBoard: [[Int]] { boardMatrix }
     
-    required init(boardSize: Int) {
-        boardMatrix = Array(repeating: Array(repeating: 0, count: Int(boardSize)), count: Int(boardSize))
+    var currentBoard: [[Int]] {
+        boardMatrix
     }
     
-    func place(figure: Figure) throws(PlacementError) -> PlacementResult {
+    var boardSize: Int {
+        boardMatrix.count
+    }
+    
+    var occupiedPosistions: [Position] {
+        figures.map { $0.position }
+    }
+    
+    required init(boardSize: Int) {
+        boardMatrix = Array(repeating: Array(repeating: 0,
+                                             count: boardSize),
+                            count: boardSize)
+    }
+    
+    func place(figure: Figure) throws -> PlacementResult {
         let size = boardMatrix.count
+        
         guard figure.position.x < size, figure.position.y < size else {
-            throw .outsideBounds
+            throw PlacementError.outsideBounds
         }
+        
         guard !figures.contains(where: { $0.position == figure.position }) else {
-            throw .occupied
+            throw PlacementError.occupied
         }
+        
         let possibleMoves = figure.availableMovementFields(boardMatrix)
+        
         for figure in figures {
-            if possibleMoves[Int(figure.position.x)][Int(figure.position.y)] == 1 {
-                throw .invalid
+            if possibleMoves[figure.position.x][figure.position.y] == 1 {
+                throw PlacementError.invalid
             }
         }
+        
         figures.append(figure)
         boardMatrix = boardMatrix.merge(with: possibleMoves)
-        return .init(newBoard: boardMatrix, occupiedPositions: figures.map { $0.position })
+        
+        return PlacementResult(newBoard: boardMatrix, occupiedPositions: occupiedPosistions)
+    }
+    
+    func reset() {
+        figures.removeAll()
+        boardMatrix = Array(repeating: Array(repeating: 0,
+                                             count: boardSize),
+                            count: boardSize)
+    }
+    
+    func remove(at position: Position) -> PlacementResult {
+        if let index = figures.firstIndex(where: { $0.position == position }) {
+            figures.remove(at: index)
+        }
+        
+        boardMatrix = Array(repeating: Array(repeating: 0,
+                                             count: boardSize),
+                            count: boardSize)
+        
+        for figure in figures {
+            let moves = figure.availableMovementFields(boardMatrix)
+            boardMatrix = boardMatrix.merge(with: moves)
+        }
+        
+        return PlacementResult(newBoard: boardMatrix, occupiedPositions: occupiedPosistions)
     }
 }
 
