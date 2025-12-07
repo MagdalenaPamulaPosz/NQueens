e44768c94ecba66954164436dec1e067f4cd2258
MagdalenaPamulaPosz <mpamula92@gmail.com>
2025-12-07 19:13:46 +0100

    Remove previous implementation

--- NQueens/NQueens/SquareView.swift
+++ NQueens/NQueens/SquareView.swift
@@ -52,48 +52,6 @@ struct SquareView: View {
     }
 }
 
-struct ChessBoardView: View {
-    let moves: [[Int]]
-    let queens: [Position]
-    let onTap: (Int, Int) -> Void
-    
-    var body: some View {
-        VStack {
-            LazyVGrid(
-                columns: Array(repeating: GridItem(.flexible()), count: moves.count),
-                spacing: 2
-            ) {
-                ForEach(0..<moves.count * moves.count, id: \.self) { index in
-                    let i = index / moves.count
-                    let j = index % moves.count
-                    
-                    let isQueen = queens.contains(where: { $0.x == i && $0.y == j })
-                    let isMove = moves[i][j] == 1
-                    
-                    ZStack {
-                        
-                        Rectangle()
-                            .fill(isMove ? Color.green.opacity(0.45) :
-                               (i + j).isMultiple(of: 2)
-                               ? Color.gray.opacity(0.3)
-                               : Color.gray.opacity(0.1))
-                        
-                        if isQueen {
-                            Text("♛")
-                                .font(.system(size: 24))
-                        }
-                        
-                    }
-                    .aspectRatio(1, contentMode: .fit)
-                    .onTapGesture {
-                        onTap(i, j)
-                    }
-                }
-            }
-        }
-    }
-}
-
 //#Preview {
 //    SquareView()
 //}
