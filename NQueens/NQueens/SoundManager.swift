c491eb042863ea86e0f6116150636224f0420577
MagdalenaPamulaPosz <mpamula92@gmail.com>
2025-12-09 15:59:44 +0100

    Add SoundManager

---
+++ NQueens/NQueens/SoundManager.swift
@@ -0,0 +1,26 @@
+//
+//  SoundManager.swift
+//  NQueens
+//
+//  Created by Magdalena Pamuła-Posz on 09/12/2025.
+//
+
+import AVFoundation
+
+class SoundManager {
+    static let shared = SoundManager()
+    
+    private var player: AVAudioPlayer?
+    
+    func playSound(fileName: String) {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "wav") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.prepareToPlay()
+            player?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+}
