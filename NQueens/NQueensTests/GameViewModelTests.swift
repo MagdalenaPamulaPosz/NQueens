b04b1f83b42778343671131b589a3e39b0992a1e
MagdalenaPamulaPosz <mpamula92@gmail.com>
2025-12-09 23:03:17 +0100

    Add view model tests

---
+++ NQueens/NQueensTests/GameViewModelTests.swift
@@ -0,0 +1,121 @@
+//
+//  GameViewModelTests.swift
+//  NQueens
+//
+//  Created by Magdalena Pamuła-Posz on 09/12/2025.
+//
+
+import XCTest
+@testable import NQueens
+
+final class GameViewModelTests: XCTestCase {
+
+    func testNewGameInitializesState() {
+        let feedbackProvider = MockFeedbackProvider()
+        let vm = GameViewModel(feedbackProvider: feedbackProvider)
+        
+        vm.boardSize = 4
+        vm.newGame()
+        
+        XCTAssertNotNil(vm.game)
+        XCTAssertEqual(vm.effectiveBoardSize, 4)
+        XCTAssertEqual(vm.currentBoard.count, 4)
+        XCTAssertTrue(vm.queenPositions.isEmpty)
+        XCTAssertNotNil(vm.startDate)
+        XCTAssertNil(vm.lastElapsed)
+    }
+    
+    func testRestartClearsQueensAndResetsTimer() {
+        let feedbackProvider = MockFeedbackProvider()
+        let vm = GameViewModel(feedbackProvider: feedbackProvider)
+        
+        vm.boardSize = 4
+        vm.newGame()
+
+        vm.handleTap(x: 0, y: 0)
+        XCTAssertEqual(vm.queenPositions.count, 1)
+
+        vm.restart()
+        
+        XCTAssertEqual(vm.queenPositions.count, 0)
+        XCTAssertNotNil(vm.startDate)
+        XCTAssertNil(vm.lastElapsed)
+        
+        for row in vm.currentBoard {
+            XCTAssertTrue(row.allSatisfy { $0 == 0 })
+        }
+    }
+    
+    func testHandleTapPlacesQueen() {
+        let feedbackProvider = MockFeedbackProvider()
+        let vm = GameViewModel(feedbackProvider: feedbackProvider)
+        
+        vm.boardSize = 4
+        vm.newGame()
+        
+        vm.handleTap(x: 0, y: 0)
+        XCTAssertEqual(vm.queenPositions.count, 1)
+        XCTAssertEqual(feedbackProvider.placeCount, 1)
+    }
+    
+    func testHandleTapRemovesQueenWhenAlreadyOccupied() {
+        let feedbackProvider = MockFeedbackProvider()
+        let vm = GameViewModel(feedbackProvider: feedbackProvider)
+        
+        vm.boardSize = 4
+        vm.newGame()
+
+        vm.handleTap(x: 0, y: 0)
+        XCTAssertEqual(vm.queenPositions.count, 1)
+        XCTAssertEqual(feedbackProvider.placeCount, 1)
+
+        vm.handleTap(x: 0, y: 0)
+        XCTAssertEqual(vm.queenPositions.count, 0)
+        XCTAssertEqual(feedbackProvider.removeCount, 1)
+    }
+    
+    func testInvalidMoveTriggersShakeAndErrorFeedback() {
+        let feedbackProvider = MockFeedbackProvider()
+        let vm = GameViewModel(feedbackProvider: feedbackProvider)
+        
+        vm.boardSize = 4
+        vm.newGame()
+        
+        vm.handleTap(x: 0, y: 0)
+        XCTAssertEqual(vm.queenPositions.count, 1)
+        XCTAssertEqual(feedbackProvider.placeCount, 1)
+        
+        let previousShake = vm.shakeValue
+        
+        vm.handleTap(x: 0, y: 1)
+        
+        XCTAssertEqual(vm.queenPositions.count, 1)
+        XCTAssertTrue(vm.shakeValue > previousShake)
+        XCTAssertEqual(feedbackProvider.errorCount, 1)
+        XCTAssertEqual(feedbackProvider.errorNotifications, 1)
+    }
+    
+    func testWinningUpdatesBestTimeAndShowsWinScreen() {
+        let feedbackProvider = MockFeedbackProvider()
+        let vm = GameViewModel(feedbackProvider: feedbackProvider)
+        
+        vm.boardSize = 4
+        vm.newGame()
+        
+        vm.handleTap(x: 0, y: 1)
+        vm.handleTap(x: 1, y: 3)
+        vm.handleTap(x: 2, y: 0)
+        vm.handleTap(x: 3, y: 2)
+        
+        XCTAssertEqual(vm.queenPositions.count, 4)
+        XCTAssertTrue(vm.shouldShowWinScreen)
+        XCTAssertNotNil(vm.lastElapsed)
+        
+        let best = vm.bestTimes[vm.effectiveBoardSize]
+        XCTAssertNotNil(best)
+        
+        // win feedback
+        XCTAssertEqual(feedbackProvider.winCount, 1)
+        XCTAssertEqual(feedbackProvider.successNotifications, 1)
+    }
+}
